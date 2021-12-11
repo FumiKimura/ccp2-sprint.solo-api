7b1bb73f5e58bd2fe113cb300f43395ec7660328
Norifumi Kimura <norical496@gmail.com>
2021-12-12 03:26:01 +0900

    Added submission feature

--- src-frontend/src/App.tsx
+++ src-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactJson from 'react-json-view';
 import './App.css';
 import Navigation from './navigation/navigation';
@@ -10,19 +10,47 @@ function App() {
     "hello3":"heyhey"
   };
 
+  //Hooks
+  const [request, setRequest] = useState("GET");
+  const [path, setPath] = useState("");
+
+  //Handler
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>){  
+    event.preventDefault();
+    switch (path) {
+      case "GET":
+        break;
+      case "POST":
+        break;
+      case "PATCH":
+        break;
+      case "DELETE":
+        break;
+    }
+  }
+
+  const handleRequestChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setRequest(event.target.value);
+  }
+
+  const handlePathChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPath(event.target.value);
+  }
+  
+  //HTML
   return (
     <div className="App">
       <Navigation />
       <h1 className="title">DORA API</h1>
-      <form>
-        <label className="pathLabel">https://url/api/ </label>
-        <select >
+      <form onSubmit={handleSubmit}>
+        <select onChange={handleRequestChange}>
             <option value="GET">GET</option>
             <option value="POST">POST</option>
             <option value="PATCH">PATCH</option>
             <option value="DELETE">DELETE</option>
-          </select>
-        <input className="pathInput" type="text" id="path"></input>
+        </select>
+        <label className="pathLabel">https://url/api/ </label>
+        <input className="pathInput" type="text" id="path" onChange={handlePathChange}></input>
         <input className="submitBtn" type="submit"></input>
       </form>
       <div className="jsonViewContainer">
