165ae26ed81fa1d93e0de8cb12013a86fe896b6c
Norifumi Kimura <norical496@gmail.com>
2021-12-16 02:24:19 +0900

    Removed login buttons

--- src-frontend/src/navigation/navigation.tsx
+++ src-frontend/src/navigation/navigation.tsx
@@ -13,13 +13,11 @@ function Navigation(prop: NavigationProps) {
     prop.setJSON({});
   }
   //HTML
-    return (
-      <div className="navigation">
-        <button className="signInBtn" type="button">Login</button>
-        <button className="signUpBtn" type="button">Sign up</button>
-        <button className="resetBtn" type="button" onClick={handleReset}>Reset</button>
-      </div>
-    );
+  return (
+    <div className="navigation">
+      <button className="resetBtn" type="button" onClick={handleReset}>Reset</button>
+    </div>
+  );
 }
 
 export default Navigation;
